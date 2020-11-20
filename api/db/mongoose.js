09488b1b0f9c093418eb92e7de13b96d2e3e69d0
Kaeittiwut Thongwhan <kaeitiwutt@gmail.com>
2020-11-20 14:12:32 +0700

    create backend to provide API for frontend

---
+++ api/db/mongoose.js
@@ -0,0 +1,26 @@
+// This file will handle connection logic to the MongoDB database
+
+const mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+mongoose
+  .connect('mongodb://localhost:27017/TaskManager', {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useCreateIndex: true,
+  })
+  .then(() => {
+    console.log('Connected to MongoDB successfully :)');
+  })
+  .catch((e) => {
+    console.log('Error while attempting to connect to MongoDB');
+    console.log(e);
+  });
+
+// To prevent deprectation warnings (from MongoDB native driver)
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+
+module.exports = {
+  mongoose,
+};
